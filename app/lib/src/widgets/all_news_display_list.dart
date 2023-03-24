3169084bef8872e0f8c1d5d45554194d46b77880
Priyankara Athapaththu <chamidupriyankara.sl@gmail.com>
2023-03-25 01:18:57 +0530

    Added the all news display list

---
+++ app/lib/src/widgets/all_news_display_list.dart
@@ -0,0 +1,121 @@
+// ignore_for_file: use_key_in_widget_constructors
+
+import 'package:Kamataha/src/res/custom_colors.dart';
+import 'package:Kamataha/src/screens/display_all.dart';
+import 'package:Kamataha/src/screens/display_news.dart';
+import 'package:Kamataha/src/utils/database.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class AllNewsDisplayList extends StatefulWidget {
+  const AllNewsDisplayList({required User user}) : _user = user;
+  final User _user;
+
+  @override
+  State<AllNewsDisplayList> createState() => _AllNewsDisplayListState();
+}
+
+class _AllNewsDisplayListState extends State<AllNewsDisplayList> {
+  late User _user;
+
+  @override
+  void initState() {
+    _user = widget._user;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: Database.readNews(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return const Text('Something went wrong');
+        } else if (snapshot.hasData || snapshot.data != null) {
+          if (snapshot.data!.docs.isEmpty) {
+            return Center(
+              child: Text(
+                "No news yet",
+                style: TextStyle(color: CustomColors.firebaseGrey),
+              ),
+            );
+          }
+          return ListView.separated(
+            separatorBuilder: (context, index) => const SizedBox(height: 16.0),
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) {
+              var noteInfo = snapshot.data!.docs[index].data() as dynamic;
+              String docID = snapshot.data!.docs[index].id;
+              String title = noteInfo["title"];
+              String description = noteInfo['description'];
+
+              String time = noteInfo['time'];
+
+              return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Ink(
+                    decoration: BoxDecoration(
+                      color: Color(0xFF9CF29A),
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    child: ListTile(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      onTap: () => Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => DisplayNews(
+                            currentTitle: title,
+                            currentDescription: description,
+                            documentId: docID,
+                            description: '',
+                          ),
+                        ),
+                      ),
+                      title: Text(
+                        title,
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      subtitle: Column(
+                        children: [
+                          Text(
+                            description,
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          const Text(""),
+                          // Text(
+                          //   user +
+                          //       " : " +
+                          //       time.substring(0, 20) +
+                          //       " ❤️ " +
+                          //       likes.toString(),
+                          //   maxLines: 1,
+                          //   overflow: TextOverflow.ellipsis,
+                          //   style: const TextStyle(fontSize: 10),
+                          // ),
+                        ],
+                      ),
+                      trailing: const Padding(
+                        padding: EdgeInsets.only(top: 5),
+                        child: Icon(Icons.arrow_forward_ios_sharp),
+                      ),
+                    ),
+                  ));
+            },
+          );
+        }
+
+        return Center(
+          child: CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(
+              CustomColors.firebaseOrange,
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
